a57aa12c28c277552b04153e824f84e24871d111
Nirmal Ganesh Yarramaneni <nirmalganesh11@gmail.com>
2023-06-04 12:57:29 +0530

    second commit

--- src/ArraysPractice/linkedlistpractise.java
+++ src/ArraysPractice/linkedlistpractise.java
@@ -1,5 +1,9 @@
 package ArraysPractice;
 
+import java.util.LinkedList;
+import java.util.*;
+import java.math.*;
+import java.lang.*;
 public class linkedlistpractise {
     public static class node{
         int data;
@@ -11,8 +15,20 @@ public class linkedlistpractise {
     }
     public static void main(String[] args) {
         int arr[] ={2,6,8,1,3,2,5};
-        node head = constructList(arr);
-        printlist(head);
+        //node head = constructList(arr);
+        //node reversehead = reverseList(head);
+        //printlist(reversehead);
+        ArrayList<Long> ob  = new ArrayList<Long>();
+        while(true){
+           ob.add(2343423452545L);
+            System.out.println("asdfasdfa");
+        }
+    }
+    public static node removeFirst(node head){
+        node temp = head;
+        temp = temp.next;
+        head = temp;
+        return head;
     }
     public static void printlist(node head){
         if(head == null) return;
@@ -21,6 +37,16 @@ public class linkedlistpractise {
             System.out.print(traverse.data+"->");
             traverse = traverse.next;
         }
+    }
+    public static node removelastelement(node head){
+        node temphead = head;
+        if(temphead == null) return null;
+        if(temphead.next == null) return null;
+        while(temphead.next.next!= null){
+            temphead = temphead.next;
+        }
+        temphead.next = null;
+        return head;
     }
     public static node constructList(int arr[]){
         node head =null;
@@ -37,6 +63,36 @@ public class linkedlistpractise {
                 prevPointer = prevPointer.next;
             }
         }
+        return head;
+    }
+    public static node reverseList(node head){
+//        node temp = head;
+//        node secondListHead = null;
+//        while(temp.next != null){
+//            int data = temp.data;
+//            if(secondListHead == null){
+//                node new_node = new node(data);
+//                secondListHead = new_node;
+//                continue;
+//            }
+//            node new_node = new node(data);
+//            new_node.next = secondListHead;
+//            secondListHead = new_node;
+//            temp = temp.next;
+//        }
+//        return secondListHead;
+        if(head == null) return null;
+       // node headptr = head;
+        node prev = null;
+        node curr = head;
+        node next = null;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr= next;
+        }
+        head = prev;
         return head;
     }
 }
