099cb73704b5762c9173279008c512a784063760
Nirmal Ganesh Yarramaneni <nirmalganesh11@gmail.com>
2023-06-05 16:19:27 +0530

    heart aches please help me

---
+++ src/ArraysPractice/TreesPractise.java
@@ -0,0 +1,43 @@
+package ArraysPractice;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TreesPractise {
+    public static class Treenode{
+        int data;
+        Treenode left;
+        Treenode right;
+        public Treenode(){
+
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8};
+
+
+
+
+    }
+    public static ArrayList<Integer> levelOrder(Treenode root){
+        ArrayList<Integer> ob = new ArrayList<Integer>();
+//        Queue<Treenode> q = new LinkedList<Treenode>();
+//        if(root == null)
+//            return ob;
+//        while(!q.isEmpty()){
+//            Treenode curr = q.poll();
+//            ob.add(curr.data);
+//            if(curr.left!=null)
+//                q.add(curr.left);
+//            if(curr.right!=null)
+//                q.add(root.right);
+//        }
+//
+//        return ob;
+//        Stack <TreeNode> stk = new Stack<TreeNode>();
+//        if(root == null) return ans;
+//        stk
+
+    }
+}
